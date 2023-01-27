e52c358954e753206220fb7e865bf8835499ccb7
Mateus Filipe de Lima Souza <mateus.filipe@sistemastecnol.local>
2023-01-27 14:56:11 -0300

    Add 07 diretivas

---
+++ curso_vue_3/07_diretivas/src/components/Info.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Não estou trabalhando no momento.</p>
+    <p>Utilizo as seguintes tecnologias:</p>
+    <ul>
+      <li>Vue.js</li>
+      <li>Python</li>
+      <li>Php</li>
+    </ul>
+    <p v-show="mostrar_email">Manda uma mensagem para: {{ email }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "Info",
+  data() {
+    return {
+      esta_trabalhando: false,
+      mostrar_email: true,
+      email: "mateus@sample.com",
+    };
+  },
+};
+</script>
