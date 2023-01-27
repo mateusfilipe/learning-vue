bc40600b6e3cc2408c98d6f7563445c5011cd2f5
Mateus Filipe de Lima Souza <mateus.filipe@sistemastecnol.local>
2023-01-27 11:53:10 -0300

    Add exemplo componente dado vue 04

---
+++ curso_vue_3/04_dados_componentes/src/components/PrimeiroComponente.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Ola Vue!</h1>
+    <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "PrimeiroComponente",
+  data() {
+    return {
+      nome: "Mateus",
+      profissao: "Programador",
+    };
+  },
+};
+</script>
