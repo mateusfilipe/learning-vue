87da3e61ffb0832b5205d56a58ed388561e397f4
Mateus Filipe de Lima Souza <mateus.filipe@sistemastecnol.local>
2023-01-27 15:22:43 -0300

    Add new files 08 atributos dinamicos

---
+++ curso_vue_3/08_atributos_dinamicos/src/components/Info.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Não estou trabalhando no momento.</p>
+    <p>Utilizo as seguintes tecnologias:</p>
+    <ul>
+      <li>Vue.js</li>
+      <li>Python</li>
+      <li>Php</li>
+    </ul>
+    <p v-show="mostrar_email">Manda uma mensagem para: {{ email }}</p>
+    <p>
+      Para acessar meu portfólio
+      <a v-bind:href="meu_link" target="_blank">clique aqui</a>.
+    </p>
+    <Picture />
+  </div>
+</template>
+<script>
+import Picture from './Picture.vue'
+
+export default {
+  name: "Info",
+  components: {
+    Picture,
+  },
+  data() {
+    return {
+      esta_trabalhando: false,
+      mostrar_email: true,
+      email: "mateus@sample.com",
+      meu_link: "https://google.com",
+    };
+  },
+};
+</script>
