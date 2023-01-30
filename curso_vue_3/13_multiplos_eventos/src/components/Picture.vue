827f312cc6c963ef691d57be2e32fa4eb0a66609
Mateus Filipe de Lima Souza <mateus.filipe@sistemastecnol.local>
2023-01-30 11:11:19 -0300

    Add files 13 multiplos eventos

---
+++ curso_vue_3/13_multiplos_eventos/src/components/Picture.vue
@@ -0,0 +1,14 @@
+<template>
+  <img :src="avatar" :alt="descricao" width="80" />
+</template>
+<script>
+export default {
+  name: "Picture",
+  data() {
+    return {
+      avatar: "https://cdn-icons-png.flaticon.com/512/147/147142.png",
+      descricao: "Logo Vue",
+    };
+  },
+};
+</script>
