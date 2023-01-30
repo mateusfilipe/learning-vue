dd6cd070a1a89be0615eed3f018b14c0375ebb39
Mateus Filipe de Lima Souza <mateus.filipe@sistemastecnol.local>
2023-01-30 10:30:59 -0300

    Add files ex 12 submit e click

---
+++ curso_vue_3/12_submit_click/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Projetos</a></li>
+    <li><a href="#">Contatos</a></li>
+  </ul>
+</template>
+<script>
+export default {
+  name: "Header",
+};
+</script>
+<style scoped>
+ul {
+  list-style: none;
+  display: flex;
+}
+li {
+  margin-right: 10px;
+}
+a {
+  color: #DDD;
+  text-decoration: none;
+  transition: 0.5s;
+}
+a:hover {
+  color: #000;
+  background-color: #FFF;
+}
+</style>
