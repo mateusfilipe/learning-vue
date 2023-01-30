817904c609bbab9472831b6e7d4efe4655040b51
Mateus Filipe de Lima Souza <mateus.filipe@sistemastecnol.local>
2023-01-30 14:09:02 -0300

    Add files 14 reutilizando componentes

---
+++ curso_vue_3/14_reutilizacao_componentes/src/components/Form.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h2>Formulário</h2>
+    <form action="" @submit="enviarFormulario($event)">
+      <div>
+        <input type="text" v-model="name" />
+      </div>
+      <div>
+        <input type="email" v-model="email" />
+      </div>
+      <div>
+        <Submit />
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+//import InputText from "./form/InputText.vue";
+import Submit from "./form/Submit.vue";
+export default {
+  name: "Form",
+  components: {
+    //InputText,
+    Submit,
+  },
+  data() {
+    return {
+      name: "",
+      email: "",
+    };
+  },
+  methods: {
+    enviarFormulario(e) {
+      e.preventDefault();
+
+      const name = this.name;
+      const email = this.email;
+
+      console.log("Formulário enviado");
+      console.log("Nome:" + name);
+      console.log("Email:" + email);
+    },
+  },
+};
+</script>
---
+++ curso_vue_3/14_reutilizacao_componentes/src/components/PrimeiroComponente.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Olá Vue!</h1>
+  <Reutilizacao />
+  <Reutilizacao />
+  <Reutilizacao />
+  <Reutilizacao />
+  <MultiplosEventos />
+</template>
+
+<script>
+import MultiplosEventos from "./MultiplosEventos.vue";
+import Reutilizacao from "./Reutilizacao.vue";
+export default {
+  components: {
+    MultiplosEventos,
+    Reutilizacao,
+  },
+  name: "PrimeiroComponente",
+};
+</script>
